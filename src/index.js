e0d540dbc161f44326187d2a85acdd2a19a41904
misao <mi3aaao159@gmail.com>
2021-09-10 00:49:31 +0900

    :sparkles: Add swiper JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './scss/styles.scss'
 import clickHeader from './js/modules/header'
+import mainVisualSwiper from './js/modules/mv'
 
 document.addEventListener('DOMContentLoaded', () => {
   clickHeader()
+
+  if (document.body.classList.contains('top')) {
+    mainVisualSwiper()
+  }
 })
